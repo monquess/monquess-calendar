c3d372d761e98a2f3aef9aa7277c7f38afeec5cb
Prov258 <prov.offcl@gmail.com>
2025-02-26 16:54:03 +0200

    feat: add seeder

---
+++ backend/prisma/seed/factories/user.factory.ts
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker';
+import * as bcrypt from 'bcryptjs';
+import { User } from '@prisma/client';
+import Factory from './abstract.factory';
+
+class UserFactory extends Factory<User> {
+	constructor(count: number = 10) {
+		super(count);
+		this.create();
+	}
+
+	create() {
+		for (let i = 0; i < this._count; i++) {
+			this._data.push({
+				username: faker.internet.username(),
+				email: faker.internet.email(),
+				password: bcrypt.hashSync('password', 10),
+				verified: faker.datatype.boolean(0.75),
+			} as User);
+		}
+	}
+}
+
+export default UserFactory;
