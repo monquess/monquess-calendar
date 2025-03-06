ed3547d129775af039dbf6bc4e17b75c488f8592
neffarrty <egorkovtun8@gmail.com>
2025-03-06 10:37:23 +0200

    refactor(config): replace Joi validation with class-validator

---
+++ backend/src/config/env/environment-variables.config.ts
@@ -0,0 +1,108 @@
+import {
+	IsEnum,
+	IsString,
+	IsNumber,
+	Max,
+	Min,
+	validateSync,
+} from 'class-validator';
+import { plainToInstance, Transform } from 'class-transformer';
+
+export enum Environment {
+	DEVELOPMENT = 'development',
+	PRODUCTION = 'production',
+}
+
+export class EnvironmentVariables {
+	@IsEnum(Environment)
+	readonly NODE_ENV: Environment = Environment.DEVELOPMENT;
+
+	@IsNumber()
+	@Min(0)
+	@Max(65535)
+	@Transform(({ value }) => Number(value))
+	readonly PORT: number = 3000;
+
+	@IsString()
+	readonly APP_URL: string;
+
+	@IsString()
+	readonly DATABASE_USER: string;
+
+	@IsString()
+	readonly DATABASE_PASSWORD: string;
+
+	@IsString()
+	readonly DATABASE_HOST: string;
+
+	@IsNumber()
+	@Min(0)
+	@Max(65535)
+	@Transform(({ value }) => Number(value))
+	readonly DATABASE_PORT: number;
+
+	@IsString()
+	readonly DATABASE_NAME: string;
+
+	@IsString()
+	readonly DATABASE_URL: string;
+
+	@IsString()
+	readonly REDIS_HOST: string;
+
+	@IsNumber()
+	@Min(0)
+	@Max(65535)
+	@Transform(({ value }) => Number(value))
+	readonly REDIS_PORT: number;
+
+	@IsString()
+	readonly REDIS_PASSWORD: string;
+
+	@IsString()
+	readonly JWT_ACCESS_SECRET: string;
+
+	@IsNumber()
+	@Transform(({ value }) => Number(value))
+	readonly JWT_ACCESS_EXPIRATION: number;
+
+	@IsString()
+	readonly JWT_REFRESH_SECRET: string;
+
+	@IsNumber()
+	@Transform(({ value }) => Number(value))
+	readonly JWT_REFRESH_EXPIRATION: number;
+
+	@IsString()
+	readonly S3_ACCESS_KEY_ID: string;
+
+	@IsString()
+	readonly S3_SECRET_ACCESS_KEY: string;
+
+	@IsString()
+	readonly S3_REGION: string;
+
+	@IsString()
+	readonly S3_BUCKET_NAME: string;
+
+	@IsString()
+	readonly S3_ENDPOINT: string;
+
+	@IsString()
+	readonly DEFAULT_AVATAR_PATH: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+	const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+		enableImplicitConversion: true,
+	});
+	const errors = validateSync(validatedConfig, {
+		skipMissingProperties: false,
+	});
+
+	if (errors.length > 0) {
+		throw new Error(errors.toString());
+	}
+
+	return validatedConfig;
+}
--- backend/src/modules/app.module.ts
+++ backend/src/modules/app.module.ts
@@ -1,19 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { envSchema } from '@config/env/env.schema';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { S3Module } from './s3/s3.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from './auth/auth.module';
+import { validate } from '@config/env/environment-variables.config';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
 			envFilePath: `.env.${process.env.NODE_ENV}`,
-			validationSchema: envSchema,
+			validate,
+			validationOptions: {
+				abortEarly: true,
+			},
+		}),
+		JwtModule.register({
+			global: true,
 		}),
 		PrismaModule,
 		UserModule,
+		AuthModule,
 		S3Module,
 	],
 })
