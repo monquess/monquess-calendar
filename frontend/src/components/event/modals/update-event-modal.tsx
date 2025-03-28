2081097415f5cb594ac7d19dfee7c38346d8bd32
neffarrty <egorkovtun8@gmail.com>
2025-03-28 03:42:07 +0200

    fix: add form reseting in event update

--- frontend/src/components/event/modals/update-event-modal.tsx
+++ frontend/src/components/event/modals/update-event-modal.tsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react'
 import {
 	Button,
+	Checkbox,
 	ColorInput,
 	Divider,
 	Grid,
+	Group,
 	Modal,
 	Select,
 	Stack,
 	Text,
 	TextInput,
 } from '@mantine/core'
-import { DateInput } from '@mantine/dates'
 import { useForm, zodResolver } from '@mantine/form'
 
 import { IoMdTime, IoIosColorPalette } from 'react-icons/io'
@@ -28,6 +30,14 @@ import { createEventSchema } from '@/shared/validations'
 import { useResponsive } from '@/hooks/use-responsive'
 
 import { capitalize } from 'lodash'
+import DatetimeInput from '../datetime-input'
+
+const eventTypes = Object.entries(EventType)
+	.filter(([value]) => value !== EventType.HOLIDAY)
+	.map(([value, label]) => ({
+		value,
+		label: capitalize(label),
+	}))
 
 interface UpdateEventModalProps {
 	opened: boolean
@@ -47,14 +57,6 @@ const UpdateEventModal: React.FC<UpdateEventModalProps> = ({
 
 	const form = useForm({
 		mode: 'uncontrolled',
-		initialValues: {
-			name: event.title,
-			description: event.extendedProps.description,
-			type: event.extendedProps.type,
-			startDate: event.start,
-			endDate: event.end,
-			color: event.backgroundColor,
-		},
 		validate: zodResolver(createEventSchema),
 	})
 
@@ -68,6 +70,7 @@ const UpdateEventModal: React.FC<UpdateEventModalProps> = ({
 
 			calendarRef.current?.getApi().getEventById(event.id)?.remove()
 			calendarRef.current?.getApi().addEvent(mapEvent(data))
+			form.reset()
 			showNotification(
 				'Event update',
 				'Event has been successfully updated.',
@@ -83,10 +86,27 @@ const UpdateEventModal: React.FC<UpdateEventModalProps> = ({
 		}
 	}
 
+	useEffect(() => {
+		if (event) {
+			form.setValues({
+				name: event.title,
+				description: event.extendedProps.description,
+				type: event.extendedProps.type,
+				startDate: event.start,
+				endDate: event.end,
+				color: event.backgroundColor,
+				allDay: event.allDay,
+			})
+		}
+	}, [event])
+
 	return (
 		<Modal
 			opened={opened}
-			onClose={onClose}
+			onClose={() => {
+				form.reset()
+				onClose()
+			}}
 			title="Update event"
 			size={isMobile ? 'sm' : 'md'}
 			centered
@@ -128,26 +148,30 @@ const UpdateEventModal: React.FC<UpdateEventModalProps> = ({
 							<IoMdTime size={20} />
 						</Grid.Col>
 						<Grid.Col span={6}>
-							<DateInput
-								key={form.key('startDate')}
-								{...form.getInputProps('startDate')}
-								popoverProps={{
-									zIndex: 1100,
-									position: 'bottom',
-								}}
-								w="100%"
+							<DatetimeInput
+								value={form.getValues().startDate}
+								onChange={(value) => form.setFieldValue('startDate', value)}
+								withTime={!form.getValues().allDay}
 							/>
 						</Grid.Col>
 						<Grid.Col span={6}>
-							<DateInput
-								key={form.key('endDate')}
-								{...form.getInputProps('endDate')}
-								popoverProps={{
-									zIndex: 1100,
-									position: 'bottom',
-								}}
-								w="100%"
+							<DatetimeInput
+								value={form.getValues().endDate}
+								onChange={(value) => form.setFieldValue('endDate', value)}
+								withTime={!form.getValues().allDay}
 							/>
+						</Grid.Col>
+						<Grid.Col span={1} />
+						<Grid.Col span={12}>
+							<Group align="center" mb="xs">
+								<Checkbox
+									label="All day"
+									checked={form.getValues().allDay}
+									onChange={(event) =>
+										form.setFieldValue('allDay', event.currentTarget.checked)
+									}
+								/>
+							</Group>
 						</Grid.Col>
 						<Grid.Col span={1}>
 							<IoIosColorPalette size={20} />
@@ -162,21 +186,14 @@ const UpdateEventModal: React.FC<UpdateEventModalProps> = ({
 								w="100%"
 							/>
 						</Grid.Col>
-						<Grid.Col span={1} />
-						<Grid.Col span={12}>
+						<Grid.Col span={6}>
 							<Select
-								label="Event type"
-								data={Object.entries(EventType)
-									.filter(([value]) => value !== EventType.HOLIDAY)
-									.map(([value, label]) => ({
-										value,
-										label: capitalize(label),
-									}))}
+								data={eventTypes}
 								key={form.key('type')}
 								{...form.getInputProps('type')}
 								checkIconPosition="right"
 								styles={{ dropdown: { zIndex: 1100 } }}
-								w="45%"
+								w="100%"
 							/>
 						</Grid.Col>
 					</Grid>
